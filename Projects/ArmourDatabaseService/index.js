f32bfaed8329fb7dc88cabbd9adee8e009d80b9b
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-05-23 07:30:29 +0200

    Added Sentry to Express projects

--- Projects/ArmourDatabaseService/index.js
+++ Projects/ArmourDatabaseService/index.js
@@ -1,15 +1,35 @@
+const PORT = 3002;
 const express = require('express');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
+const app = express();
 const armourDatabaseController = require('./src/controller/armourDatabaseController');  // replace with the actual path to your controller file
 
-const PORT = 3002;
-const app = express();
+Sentry.init({
+    dsn: 'https://34d73dc0fc0fb04f1c44b8ec758fce46@o4506960048881664.ingest.us.sentry.io/4507298954346496',
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app: express }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
 app.use(express.json());
 
 // Use your router for requests to a certain path
 app.use('/armourDatabase', armourDatabaseController);  // replace '/armour' with the path you want
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
